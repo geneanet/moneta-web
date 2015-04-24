833fcbce8a2eb4691270c2221b819611a447592e
Hervé BRY <herve.bry@geneanet.org>
2015-04-24 15:34:54 +0200

    More powerful filter

--- js/filters.js
+++ js/filters.js
@@ -6,22 +6,64 @@ monetaFilters.filter('taskfilter', function ($filter) {
 			return items;
 		}
 
-		expression = expression.toLowerCase()
+		expression = expression.toLowerCase().split(" ")
 
 		var result = {};
 
-		angular.forEach(items, function(task, taskid) {
-			if (task.name.toLowerCase().indexOf(expression) > -1) {
-				result[taskid] = task;
-				return;
+		function match_tag(task, tag)
+		{
+			for (var t = 0; t < task.tags.length; t++)
+			{
+				if (task.tags[t].toLowerCase().indexOf(tag) > -1)
+					return true;
 			}
 
-			angular.forEach(task.tags, function(tag) {
-				if (tag.toLowerCase().indexOf(expression) > -1) {
-					result[taskid] = task;
-					return;
+			return false;		
+		}
+
+		function match_title(task, title)
+		{
+			return task.name.toLowerCase().indexOf(title) > -1;
+		}
+
+		function xor(a,b)
+		{
+			return ( a || b ) && !( a && b );
+		}
+
+		angular.forEach(items, function(task, taskid) {
+			for (var i = 0; i < expression.length; i++)
+			{
+				var item = expression[i];
+
+				var negate = false;
+				if (item[0] == '-')
+				{
+					negate = true;
+					item = item.substr(1);
 				}
-			});
+				
+				if (item.lastIndexOf('tag:', 0) === 0)
+				{
+					item = item.substr(4);
+
+					if (xor(negate, !match_tag(task, item)))
+						return;
+				}
+				else if (item.lastIndexOf('title:', 0) === 0)
+				{
+					item = item.substr(6);
+					if (xor(negate, !match_title(task, item)))
+						return;
+				}
+				else 
+				{
+					if (xor(negate, !(match_title(task, item) || match_tag(task, item))))
+						return;
+				}
+			}
+
+			result[taskid] = task;
 		});
 
 		return result;

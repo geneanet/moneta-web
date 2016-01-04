157fd9342ff71a3b09745da99e4c371fa5ce2ca7
Hervé BRY <herve.bry@geneanet.org>
2016-01-04 17:45:08 +0100

    Common header and breadcrumb + get rid of angular-foundation

--- js/app.js
+++ js/app.js
@@ -6,10 +6,15 @@ var monetaApp = angular.module('monetaApp', [
 	'formValidation',
 	'bzm-date-picker',
 	'ngLoadingSpinner',
-	'ngAnimate'
+	'ngAnimate',
+	'ncy-angular-breadcrumb'
 ]);
 
-monetaApp.config(function($stateProvider, $urlRouterProvider) {
+monetaApp.config(function($stateProvider, $urlRouterProvider, $breadcrumbProvider) {
+	$breadcrumbProvider.setOptions({
+		templateUrl: 'templates/breadcrumb.html'
+    });
+
 	$urlRouterProvider
 		.when('/tasks/:id', '/tasks/:id/view')
 		.otherwise("/tasks");
@@ -18,48 +23,82 @@ monetaApp.config(function($stateProvider, $urlRouterProvider) {
 		.state('cluster', {
 			url: "/cluster",
 			templateUrl: "templates/cluster-status.html",
-			controller: 'ClusterStatusCtrl'
+			controller: 'ClusterStatusCtrl',
+			ncyBreadcrumb: {
+				label: 'Cluster'
+			}
 		})
 		.state('cluster-node', {
 			url: "/cluster/node/{node:[^/]+}",
 			templateUrl: "templates/node-status.html",
-			controller: 'NodeStatusCtrl'
+			controller: 'NodeStatusCtrl',
+			ncyBreadcrumb: {
+				label: 'Node {{node.name}}',
+				parent: 'cluster'
+			}
 		})
 		.state('tasks', {
 			url: "/tasks",
 			templateUrl: "templates/tasks.html",
-			controller: 'TaskListCtrl'
+			controller: 'TaskListCtrl',
+			ncyBreadcrumb: {
+				label: 'Tasks'
+			}
 		})
 		.state('newtask', {
 		 	url: "/tasks/new",
 		 	templateUrl: "templates/newtask.html",
-		 	controller: 'NewTaskCtrl'
+		 	controller: 'NewTaskCtrl',
+			ncyBreadcrumb: {
+				label: 'New Task',
+				parent: 'tasks'
+			}
 		})
 		.state('newtaskfromtemplate', {
 			url: "/tasks/new/{templateId}",
 			templateUrl: "templates/newtask.html",
-		 	controller: 'NewTaskCtrl'
+		 	controller: 'NewTaskCtrl',
+			ncyBreadcrumb: {
+				label: 'New Task',
+				parent: 'tasks'
+			}
 		})
 		.state('task', {
 			url: "/tasks/{taskId}",
 			templateUrl: "templates/task.html",
 			controller: 'TaskEditCtrl',
-			abstract: true
+			abstract: true,
+			ncyBreadcrumb: {
+				label: 'Task {{task.name}}',
+				parent: 'tasks'
+			}
 		})
 		.state('task.view', {
 			url: "/view",
 			templateUrl: "templates/task-view.html",
+			ncyBreadcrumb: {
+				label: 'Task {{task.name}}'
+			}
 		})
 		.state('task.edit', {
 			url: "/edit",
 			templateUrl: "templates/task-edit.html",
+			ncyBreadcrumb: {
+				label: 'Task {{task.name}} Edition'
+			}
 		})
 		.state('task.auditlog', {
 			url: "/auditlog",
-			templateUrl: "templates/auditlog.html"
+			templateUrl: "templates/auditlog.html",
+			ncyBreadcrumb: {
+				label: 'Task {{task.name}} Audit Log'
+			}
 		})
 		.state('auditlog', {
 			url: "/auditlog",
-			templateUrl: "templates/global-auditlog.html"
+			templateUrl: "templates/global-auditlog.html",
+			ncyBreadcrumb: {
+				label: 'Audit Log'
+			}
 		})
 });
